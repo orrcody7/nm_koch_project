aaaa54ea9f67e741bc4549afa731f70d40a5f4be
unknown <orrcody7@gmail.com>
2021-10-12 08:38:19 -0600

    created the customers and employees app and registering those apps

--- nm_koch/customers/views.py
+++ nm_koch/customers/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Customer
 
 # Create your views here.
+
+def index(request):
+    all_customers = Customer.objects.all()
+    context = {
+        'all_customers': all_customers
+    }
+    return render(request, 'customers/index.html', context)
--- nm_koch/employees/models.py
+++ nm_koch/employees/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    
